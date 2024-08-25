d0389c034fbf2b20d86bf8cd8a380cea3150ebd7
joeperpetua <qwerty.perpetua@gmail.com>
2024-08-25 19:09:45 +0200

    Add word splitting from raw text

--- src/app/songEditor/songEditor.component.ts
+++ src/app/songEditor/songEditor.component.ts
@@ -1,17 +1,32 @@
 import { Component } from "@angular/core";
 
+interface SongWord {
+    text: string,
+    chord : string
+}
+
+interface SongLine {
+    words: SongWord[]
+}
+
+interface SongSection {
+    name: string,
+    lines: SongLine[]
+}
+
 @Component({
     selector: 'SongEditor',
     standalone: true,
     templateUrl: './songEditor.component.html',
     styleUrl: './songEditor.component.css'
 })
+
 export class SongEditor {
     songTitle = '';
     songAuthor = '';
     songKey = '';
     songText = '';
-    songWordsMap = [];
+    songSections: SongSection[] = [];
     displayInfo = true;
 
     toggleInfo() {
@@ -33,5 +48,31 @@ export class SongEditor {
         this.songKey = target.value;
     }
 
+    processText() {
+        const rawText = document.querySelector('.song-source')?.querySelector('textarea')?.value;
+        if (!rawText)
+            return
 
+        let sections = rawText.split('\n\n');
+
+        let songSections: SongSection[] = [];
+        sections.forEach((section, index) => {
+            let lines = section.split('\n');
+            let sectionLines: SongLine[] = [];
+            lines.forEach(line => {
+                let words = line.split(' ');
+                let lineWords: SongWord[] = [];
+                words.forEach(word => {
+                    lineWords.push({'text': word, 'chord': ''});
+                });
+                sectionLines.push({words: lineWords})
+            })
+            songSections.push({'name': `Section ${index + 1}`, lines: sectionLines})
+        })
+        this.songSections = songSections;
+
+        console.log(rawText);
+        console.log(songSections);
+        console.log(songSections[0].lines[0].words[0].text);
+    }
 }
